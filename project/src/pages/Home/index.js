9f1fcfa6981ace0f819d6b655102b3defd753f3b
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-12-22 11:37:06 -0300

    adicionando filmes na home

--- project/src/pages/Home/index.js
+++ project/src/pages/Home/index.js
@@ -4,11 +4,14 @@ import { useEffect, useState } from 'react'
 
 import api from '../../services/api'
 
+import { Link } from 'react-router-dom'
+
+import './Home.css'
 
 
 export default function Home() {
 
-  const [filmes, setFilmes] = useState('')
+  const [filmes, setFilmes] = useState([])
 
   useEffect(()=>{
     async function loadFilmes(){
@@ -19,14 +22,26 @@ export default function Home() {
           page:1
         }
       })
-      console.log(response.data.results)
+      setFilmes(response.data.results.slice(0,10))
     }
     loadFilmes()
   },[])
 
   return (
-    <div>
-      <h1>Bem vindo a Home</h1>
+    <div className='conteiner'>
+      <div className='lista-filmes'>
+        {filmes.map((filme)=>{
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+            
+          )
+          
+        })}
+      </div>
     </div>
   )
 }
